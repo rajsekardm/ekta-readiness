474fe0489371a0031f3cf2ce567555d81d2f5593
mohan <mohan@sparkouttech.com>
2022-03-01 11:49:35 +0530

    updated

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+// Ethereum Side Contract Addresses:
+// old token - 0x039a19d716c6ade855241e2d94d01195ab094dc9
+// Migrator - 0x470B46D30ae5b9338908C9F6e991e6006565AABD
+// New token - 0xD0daD90122a5772B6d5a03916b80A7dfAa2e2EEb
+// Bridge ETH - 0x06bA4D344eA7028C97e94674CE15Ca0381311253
+
+// Binance Contract Addresses:
+// BridgeBsc: 0xaA13e6040BC6414D91eC0c4D7Ac4800f4a472B37
+// Bep-20 : 0x4C6Fc31d0ceBB562AF6328862aD0C5941638D75f
