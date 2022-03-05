3a9de420ac0bf6616999491b2064854a7074f7e1
Yokesh Sankar <yokesh@sparkouttech.com>
2022-03-05 17:12:03 +0530

    Update patch.js

--- patch.js
+++ patch.js
@@ -1,3 +1,4 @@
+// For Angular <11
 const fs = require('fs');
 const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
 
@@ -5,9 +6,10 @@ fs.readFile(f, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
- var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true, fs: 'empty', net: 'empty'}');
-  
+  var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
+
   fs.writeFile(f, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
+// ----
