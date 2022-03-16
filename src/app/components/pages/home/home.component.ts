2bd850a0b18752600d603cd57e42aa3cbe4f2df9
mohan <mohan@sparkouttech.com>
2022-03-16 13:26:47 +0530

    Updated

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -267,7 +267,7 @@ export class HomeComponent implements OnInit {
          address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
          symbol: 'USDT',
          decimals: 18,
-         image: 'https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707',
+         image: 'https://raw.githubusercontent.com/MetaMask/contract-metadata/master/images/usdt.svg',
        },
      },
    })
@@ -287,7 +287,7 @@ export class HomeComponent implements OnInit {
          address: '0x55d398326f99059fF775485246999027B3197955',
          symbol: 'USDT',
          decimals: 18,
-         image: 'https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707',
+         image: 'https://raw.githubusercontent.com/MetaMask/contract-metadata/master/images/usdt.svg',
        },
      },
    })
